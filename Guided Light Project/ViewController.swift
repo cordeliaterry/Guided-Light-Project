5cfb2294be82711b621a72e36021afc78ecd004b
Cordelia Terry <cterry25@student.ccc.edu>
2020-10-28 00:25:31 -0500

    Initial Commit

---
+++ Guided Light Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guided Light Project
+//
+//  Created by MXC Swift on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
