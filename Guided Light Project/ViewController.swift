ee592bd1edd0f3c849441836f06966667def3db8
Cordelia Terry <cterry25@student.ccc.edu>
2020-10-31 00:23:13 -0500

    Guided Light

--- Guided Light Project/ViewController.swift
+++ Guided Light Project/ViewController.swift
@@ -5,15 +5,28 @@
 //  Created by MXC Swift on 10/28/20.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view.
     }
 
-
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+        
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
 }
 
